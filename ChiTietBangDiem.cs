1e83e752af9bb25f293c81b530559aea53510949
Vinh Lưu Nguyễn Thế <22521672@ms.uit.edu.vn>
2024-05-29 15:18:04 +0700

    ChiTietBangDiem

---
+++ ChiTietBangDiem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyHocSinh_Nhom15
+{
+    sealed class ChiTietBangDiem
+    {
+        private static ChiTietBangDiem _instance;
+        public string idBangDiem;
+        public string idHocSinh;
+        public float Diem15p;
+        public float Diem1Tiet;
+        public float DiemTB;
+
+        public static ChiTietBangDiem GetInstance()
+        {
+            if (_instance == null)
+                _instance = new ChiTietBangDiem();
+            return _instance;
+        }
+
+        public void ThemDiem(float diem15p,float diem1t, float diemTB)
+        {
+            SQLConnect db = SQLConnect.GetInstance();
+            db.Open();
+            db.sqlCmd.CommandType = CommandType.Text;
+            db.sqlCmd.CommandText = "INSERT INTO ";
+        }
+    }
+}
